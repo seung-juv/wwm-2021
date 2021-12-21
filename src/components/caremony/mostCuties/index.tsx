ff2b422403658f723b0bac63bf420f5bd9f6b110
Seung Ju <kingsky32@gmail.com>
2021-12-21 23:17:04 +0900

    ✅ 연말결산 리팩토링 및 톡방의 재롱둥이 완료

--- src/components/caremony/mostCuties/index.tsx
+++ src/components/caremony/mostCuties/index.tsx
@@ -7,7 +7,7 @@ const mostCuties: Array<User> = [users[0], users[12], users[18]];
 const cuties: Array<User> = [users[7], users[11]];
 
 const MostCuties = (): React.ReactElement => {
-  return <Most subTitle="simsimee" title="톡방의 재롱둥이" mostUsers={mostCuties} users={cuties} />;
+  return <Most subTitle="Gagman" title="톡방의 재롱둥이" mostUsers={mostCuties} users={cuties} />;
 };
 
 export default MostCuties;
