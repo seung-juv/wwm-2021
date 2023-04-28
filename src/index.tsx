58388299e60e2b5e19b1380b3d79cac5425d6709
Seung Ju <kingsky32@gmail.com>
2023-04-28 14:06:23 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import useFetch from "./hooks/useFetch";
-import Timer from "./routes/timer";
 import Ceremony from "./routes/ceremony";
 import Events from "./routes/events";
 import Home from "./routes/home";
 import Top10 from "./routes/top10";
-import { restApi } from "./apis";
-import moment from "moment";
-
-export const openDate = moment("2021-12-24 12:24");
 
 function App(): React.ReactElement | null {
-  const { data, fetch, loading } = useFetch();
-
-  async function init(): Promise<void> {
-    try {
-      await fetch(restApi.get("/time"));
-    } catch (error) {
-      console.warn(error);
-    }
-  }
-
-  React.useEffect(() => {
-    init();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  if (!data || loading) {
-    return null;
-  }
-
-  if (moment(data).format("YYYYMMDDHHmmss") < openDate.format("YYYYMMDDHHmmss")) {
-    return <Timer date={data} />;
-  }
-
   return (
     <BrowserRouter>
       <Routes>
